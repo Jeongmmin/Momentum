fa52d1f515dbdb250110ae46d1b2e2ccd88ce7cc
Jeongmmin <seo88kr@hanmail.net>
2022-03-21 04:14:50 +0900

    add. 🎨Ver1.1

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
-const images = ["background1.jpg", "background2.jpg", "background3.jpg"];
+const imgBox = document.querySelector("#img__box")
+const images = ["background0.jpg", "background1.jpg", "background2.jpg", "background3.jpg", "background4.jpg", "background5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -10,5 +11,6 @@ console.log(bgImage);
 
 bgImage.src = `./img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+imgBox.appendChild(bgImage);
 
